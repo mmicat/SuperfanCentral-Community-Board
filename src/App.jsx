eb6bf9f9cc8951de56030a026189122610b33cbf
mmicat <miramohd.pro@outlook.com>
2026-06-06 02:13:37 +0400

    final touches: header styling, banner, grid cap & view fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ const App = () => {
 
   return (
     <div className="App">
+      <img className="banner" src="https://i.pinimg.com/originals/d9/5a/4f/d95a4ff74b8c961f857c5e57e6f793bb.gif" alt="Animated banner" />
       <h1>Superfan Central</h1>
       <h2>A nerd's guide to the most popular fandom-focused events in the United Arab Emirates!</h2>
       <Board />
-      
+
     </div>
   )
 }
